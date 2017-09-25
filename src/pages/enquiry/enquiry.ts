7c7dfc9c660839768c0b694ae70a40772310d1c8
Manoj Shivakumar <manojshivakumar@mobiddiction.com.au>
2017-09-25 23:41:34 +1000

    Added Android Platform to project and fixed email composer.

--- src/pages/enquiry/enquiry.ts
+++ src/pages/enquiry/enquiry.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { EmailComposer } from "@ionic-native/email-composer";
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {EmailComposer} from "@ionic-native/email-composer";
 
 /**
  * Generated class for the EnquiryPage page.
@@ -17,49 +17,34 @@ import { EmailComposer } from "@ionic-native/email-composer";
 export class EnquiryPage {
 
   productName = "";
-  phoneNumber:string;
-  place:string;
-  quantity:string;
-  name:string;
+  phoneNumber: string;
+  place: string;
+  quantity: string;
+  name: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private emailComposer: EmailComposer) {
   }
 
   ionViewDidLoad() {
-    console.log(this.navParams.get('data'));
-    // this.sendEmail();
     const data = this.navParams.get('data');
-    console.log(data);
     this.productName = data;
-    console.log(this.phoneNumber);
   }
 
-  sendEmail(productName,name,phone,place,quantity)
-  {
-    this.emailComposer.isAvailable().then((available: boolean) =>{
-      if(available) {
-        let email = {
-          to: 'avijith.naik@datakue.com',
-          cc: 'avijith90naik@gmail.com',
-          bcc: ['team.datakue@gmail.com', 'jane@doe.com'],
-          attachments: [
-            'file://img/logo.png',
-            'res://icon.png',
-            'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
-            'file://README.pdf'
-          ],
-          subject: 'Cordova Icons',
-          body: 'Product Name: '+ productName +'\n' +
-          'Name: '+name+'\n'+
-          'Phone Number: '+phone+'\n'+
-          'Place: '+place+'\n'+
-          'Quantity: '+quantity+'\n',
-          isHtml: true
-        };
-
-        this.emailComposer.open(email);
-      }
-    });
+  sendEmail(productName, name, phone, place, quantity) {
+    let email = {
+      to: 'avijith.naik@datakue.com',
+      cc: 'avijith90naik@gmail.com',
+      bcc: ['team.datakue@gmail.com', 'jane@doe.com'],
+      attachments: [],
+      subject: 'Product Enquiry',
+      body: '<p>Product Name: ' + productName + "</p>" +
+      '<p>Name: ' + name + '</p>' +
+      '<p>Phone Number: ' + phone + '</p>' +
+      '<p>Place: ' + place + '</p>' +
+      '<p>Quantity: ' + quantity + '</p>',
+      isHtml: true
+    };
+    this.emailComposer.open(email);
   }
 
 }
